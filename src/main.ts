7e6c9b87201fbf6fa32a72298bb869350d026ea6
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2024-02-24 03:57:16 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 // import cors from 'cors';
 
 async function bootstrap() {
